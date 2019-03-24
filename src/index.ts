57b9a633fbd3c555b00751066f7e8de77b8ed16e
John Sanders <john@johnsanders.tv>
2019-03-24 12:28:17 +0000

    Start adding authentication

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
 import express from 'express';
 import { createServer } from 'http';
 import cors from 'cors';
+import session from 'express-session';
+import mongoose from 'mongoose';
+import initUsers from './models/users';
+import initPassport from './config/passport';
+import routes from './routes/index';
+
+mongoose.promise = global.Promise;
+mongoose.connect('mongodb://johnsanders.tv/typeretriever');
+mongoose.set('debug', true);
+
+createUsers();
 
 const app = express();
 const server = createServer(app);
 
 app.use(cors());
+app.use(routes);
 app.use('/', express.static('/home/jon13210/www/johnsanders.tv/'));
 
 server.listen(3000, 'localhost');
